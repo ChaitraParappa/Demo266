af0bef54d6c8adc9a1552d6c85facd3c94e47fed
ChaitraParappa <chaitra.p321@gmail.com>
2023-03-08 09:44:24 +0530

    Taking user input

---
+++ DisplayUsername.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public  class DisplayUsername
+    {
+        public static string name = "Hi";
+        internal static void PrintUsername()
+        {
+            Console.WriteLine("Please enter the username");
+            string username = Console.ReadLine();
+
+            int val = username.Length;
+
+            Console.WriteLine("The Length " + val);
+
+            if (username.Length > 3)
+            {
+                Console.WriteLine("Hello " + username + " , How are you?");
+
+                Console.WriteLine("Hello {0} {1} {2}, How are you?", username, "guuu", "tttt");
+            }
+            else
+            {
+                Console.WriteLine("Invalid username");
+            }
+        }
+
+        public static void Display()
+        {
+            PrintUsername();
+        }
+    }
+}
